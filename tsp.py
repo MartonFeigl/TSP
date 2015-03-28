fc66b95a20df3cf1ef1948835b365fa88149036a
Marton Feigl <s1465355@cplab056.ph.ed.ac.uk>
2015-03-28 13:48:18 +0000

    In class Work, in state_transition_rule, convoluted code was simplified

--- tsp.py
+++ tsp.py
@@ -12,7 +12,7 @@ class Work():
         self.path_vec.append(self.start_node)
         self.path_cost = 0
         self.Beta = 1.0
-        self.randNumthreshold = 0.5
+        self.randNumThreshold = 0.5
         self.Rho = 0.99
         self.nodesToVisit = {}
         for i in range(0, self.graph.num_nodes):
@@ -22,16 +22,16 @@ class Work():
         for i in range(0, self.graph.num_nodes):
             self.path_mat.append([0] * self.graph.num_nodes)
 
-    #could this be simpler?
+    #could this be simpler? No, while loop is necessary, furthermore due to data dependencies variable reduction isn't possible
     def run(self):
         graph = self.grouping.graph
         while not self.end():
-            new_node = self.state_transition_rule(self.curr_node)
-            self.path_cost += graph.delta(self.curr_node, new_node)
-            self.path_vec.append(new_node)
-            self.path_mat[self.curr_node][new_node] = 1
-            self.local_updating_rule(self.curr_node, new_node)
-            self.curr_node = new_node
+	    new_node = self.state_transition_rule(self.curr_node)
+	    self.path_cost += graph.delta(self.curr_node, new_node)
+	    self.path_vec.append(new_node)
+	    self.path_mat[self.curr_node][new_node] = 1
+	    self.local_updating_rule(self.curr_node, new_node)
+	    self.curr_node = new_node
         self.path_cost += graph.delta(self.path_vec[-1], self.path_vec[0])
         self.grouping.update(self)
         self.__init__(self.ID, self.start_node, self.grouping)
@@ -44,7 +44,9 @@ class Work():
         graph = self.grouping.graph
         #q = random.random()
         max_node = -1
-        if random.random() < self.randNumthreshold:
+
+
+        if random.random() < self.randNumThreshold:
             print "Exploitation"
             max_val = -1
             val = None
@@ -56,25 +58,34 @@ class Work():
                     max_val = val
                     max_node = node
         else:
-            #Bob was here
+            print "Bob was here"
             print "Exploration"
             sum = 0
-            node = -1
+            #node = -1 # irrelevant, since for loop reinitialises it
+	    temp_val = 0;
+	    p = []
             for node in self.nodesToVisit.values():
                 if graph.tau(curr_node, node) == 0:
                     raise Exception("tau = 0")
-                sum += graph.tau(curr_node, node) * math.pow(graph.etha(curr_node, node), self.Beta)
+                temp_val = graph.tau(curr_node, node) * math.pow(graph.etha(curr_node, node), self.Beta)
+		sum += temp_val
+		p.append(temp_val)
             if sum == 0:
                 raise Exception("sum = 0")
             avg = sum / len(self.nodesToVisit)
             print "avg = %s" % (avg,)
+	    STRI = 0 #State Transition Rule Index, 
             for node in self.nodesToVisit.values():
-                p = graph.tau(curr_node, node) * math.pow(graph.etha(curr_node, node), self.Beta)
-                if p > avg:
-                    print "p = %s" % (p,)
+                #p = graph.tau(curr_node, node) * math.pow(graph.etha(curr_node, node), self.Beta)
+		
+                if p[STRI] > avg:
+                    print "p = %s" % (p[STRI],)
                     max_node = node
+		STRI += 1
             if max_node == -1:
                 max_node = node
+
+
         if max_node < 0:
             raise Exception("max_node < 0")
         del self.nodesToVisit[max_node]
@@ -180,7 +191,7 @@ class GraphBit:
         if tau_mat is None:
             self.tau_mat = []
             for i in range(0, num_nodes):
-                self.tau_mat.append([0] * num_nodes)
+                self.tau_mat.append([0] * num_nodes)#add num_nodes to the end of tau_mat
 
     def delta(self, r, s):
         return self.delta_mat[r][s]
@@ -259,7 +270,7 @@ def main(argv):
         print "Repetition %s" % i
         graph.reset_tau()
         workers = BigGroup(graph, numberOfAnts, numberOfIterations)
-        print "Colony Started"
+        print "Colony Started and running well"
         workers.start()
         if workers.bestPathCost < bestPathCost:
              print "Colony Path"
